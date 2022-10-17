93a507c722e4756a2e015a656e316150801b4f05
dragonir <dragonirxo@outlook.com>
2022-10-17 17:30:05 +0800

    feat(shader): 💥 增加传送门shader效果.

--- 05-shader/src/script.js
+++ 05-shader/src/script.js
@@ -2,6 +2,11 @@ import './style.css';
 import * as THREE from 'three';
 import * as dat from 'dat.gui';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass';
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer';
+import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass';
+import portalVertexShader from './shaders/portal/vertex.glsl';
+import portalFragmentShader from './shaders/portal/fragment.glsl';
 
 // 定义渲染尺寸
 const sizes = {
@@ -11,7 +16,11 @@ const sizes = {
 
 // 初始化渲染器
 const canvas = document.querySelector('canvas.webgl');
-const renderer = new THREE.WebGLRenderer({ canvas: canvas });
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+  antialias: true,
+  autoClear: false
+});
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
@@ -19,8 +28,8 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 const scene = new THREE.Scene();
 
 // 初始化相机
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.z = 3
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 10000)
+camera.position.z = 3;
 scene.add(camera);
 
 const controls = new OrbitControls(camera, renderer.domElement);
@@ -37,15 +46,6 @@ window.addEventListener('resize', () => {
   camera.aspect = sizes.width / sizes.height;
   camera.updateProjectionMatrix();
 });
-
-// 动画
-const tick = () => {
-  // 更新渲染器
-  renderer.render(scene, camera);
-  // 页面重绘时调用自身
-  window.requestAnimationFrame(tick);
-}
-tick();
 
 var options = {
   exposure: 2.8,
@@ -74,8 +74,93 @@ colors.addColor(options, 'color4').name('layer4');
 colors.addColor(options, 'color5').name('layer5');
 colors.open();
 
+const textureLoader = new THREE.TextureLoader();
 // 创建网格
-const planeGeometry = new THREE.PlaneBufferGeometry(4, 4, 1, 1);
-const planeMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
-const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+const portalGeometry = new THREE.PlaneBufferGeometry(5, 5, 1, 1);
+const portalMaterial = new THREE.ShaderMaterial({
+  uniforms: {
+    time: {
+      type: 'f',
+      value: 0.0,
+    },
+    perlinnoise: {
+      type: 't',
+      value: textureLoader.load('/images/perlinnoise.png'),
+    },
+    sparknoise: {
+      type: 't',
+      value: textureLoader.load('/images/sparknoise.png'),
+    },
+    waterturbulence: {
+      type: 't',
+      value: textureLoader.load('/images/waterturbulence.png'),
+    },
+    noiseTex: {
+      type: 't',
+      value: textureLoader.load('/images/noise.png'),
+    },
+    color5: {
+      value: new THREE.Vector3(...options.color5),
+    },
+    color4: {
+      value: new THREE.Vector3(...options.color4),
+    },
+    color3: {
+      value: new THREE.Vector3(...options.color3),
+    },
+    color2: {
+      value: new THREE.Vector3(...options.color2),
+    },
+    color1: {
+      value: new THREE.Vector3(...options.color1),
+    },
+    color0: {
+      value: new THREE.Vector3(...options.color0),
+    },
+    resolution: {
+      value: new THREE.Vector2(sizes.width, sizes.height)
+    }
+  },
+  fragmentShader: portalFragmentShader,
+  vertexShader: portalVertexShader
+});
+const plane = new THREE.Mesh(portalGeometry, portalMaterial);
 scene.add(plane);
+
+// 辉光效果
+const renderScene = new RenderPass(scene, camera);
+const bloomPass = new UnrealBloomPass(
+  new THREE.Vector2(window.innerWidth, window.innerHeight),
+  1.5,
+  .4,
+  .85
+);
+bloomPass.threshold = options.bloomThreshold;
+bloomPass.strength = options.bloomStrength;
+bloomPass.radius = options.bloomRadius;
+
+const composer = new EffectComposer(renderer);
+composer.addPass(renderScene);
+composer.addPass(bloomPass);
+
+// 更新材质
+const updateShaderMaterial = deltaTime => {
+  portalMaterial.uniforms.time.value = deltaTime / 5000;
+  portalMaterial.uniforms.color5.value = new THREE.Vector3(...options.color5);
+  portalMaterial.uniforms.color4.value = new THREE.Vector3(...options.color4);
+  portalMaterial.uniforms.color3.value = new THREE.Vector3(...options.color3);
+  portalMaterial.uniforms.color2.value = new THREE.Vector3(...options.color2);
+  portalMaterial.uniforms.color1.value = new THREE.Vector3(...options.color1);
+  portalMaterial.uniforms.color0.value = new THREE.Vector3(...options.color0);
+}
+
+// 动画
+const tick = deltaTime => {
+  updateShaderMaterial(deltaTime);
+  // 更新渲染器
+  renderer.render(scene, camera);
+  // 页面重绘时调用自身
+  window.requestAnimationFrame(tick);
+  composer.render();
+}
+tick();
