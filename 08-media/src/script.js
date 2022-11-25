ea602bdf922086410490486a898832f436aa53ae
dragonir <dragonirxo@outlook.com>
2022-11-25 19:02:19 +0800

    feat(media): 📱 添加环境贴图、渲染效果优化、资源压缩优化等.

--- 08-media/src/script.js
+++ 08-media/src/script.js
@@ -19,18 +19,17 @@ const renderer = new THREE.WebGLRenderer({
 });
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-renderer.autoClear = false;
-renderer.setClearAlpha(0);
 renderer.physicallyCorrectLights = true;
-renderer.toneMapping = THREE.CineonToneMapping;
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
 renderer.toneMappingExposure = 2;
+renderer.outputEncoding = THREE.sRGBEncoding
 
 // 初始化场景
 const scene = new THREE.Scene();
 // 初始化相机
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 10000)
-camera.position.set(0, 1, 5);
-
+camera.position.set(0, 1.2, 10);
+// 镜头控制器
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 controls.enablePan = false;
@@ -47,70 +46,81 @@ window.addEventListener('resize', () => {
   camera.updateProjectionMatrix();
 });
 
-// 光照
-const ambientLight = new THREE.AmbientLight(0xffffff, 5);
-scene.add(ambientLight);
+// 全景背景
+const cubeTextureLoader = new THREE.CubeTextureLoader();
+const environmentMap = cubeTextureLoader.load([
+  '/textures/environmentMaps/px.jpg',
+  '/textures/environmentMaps/nx.jpg',
+  '/textures/environmentMaps/py.jpg',
+  '/textures/environmentMaps/ny.jpg',
+  '/textures/environmentMaps/pz.jpg',
+  '/textures/environmentMaps/nz.jpg'
+]);
+environmentMap.encoding = THREE.sRGBEncoding;
+scene.background = environmentMap;
+scene.environment = environmentMap;
 
-const directionalLight = new THREE.DirectionalLight('#ffffff', 5);
+// 环境光
+const ambientLight = new THREE.AmbientLight(0xffffff, 2);
+scene.add(ambientLight);
+// 平行光
+const directionalLight = new THREE.DirectionalLight('#ffffff', 3);
 directionalLight.castShadow = true;
-directionalLight.shadow.camera.far = 20;
+directionalLight.shadow.camera.far = 50;
 directionalLight.shadow.mapSize.set(1024, 1024);
 directionalLight.shadow.normalBias = 0.05;
-directionalLight.position.set(1, 3, 5);
+directionalLight.position.set(1, 10, -10);
 scene.add(directionalLight);
 
+// 创建视频材质
+const video = document.getElementById('video');
+const videoTexture = new THREE.VideoTexture(video);
+
+// 模型加载
+const screen = {
+  mesh: null,
+  material: null,
+  videoMaterial: null
+};
 // 加载管理
 const loadingManager = new THREE.LoadingManager();
-loadingManager.onLoad = () => {}
-
+loadingManager.onLoad = () => { }
 // 使用 dracoLoader 加载用blender压缩过的模型
 const dracoLoader = new DRACOLoader();
 dracoLoader.setDecoderPath('/draco/');
 dracoLoader.setDecoderConfig({ type: 'js' });
 const loader = new GLTFLoader(loadingManager);
 loader.setDRACOLoader(dracoLoader);
-
-// 加载模型
-const video = document.getElementById('video');
-const videoTexture = new THREE.VideoTexture(video);
-
-const screen = {
-  mesh: null,
-  material: null,
-  videoMaterial: null
-};
-
 loader.load('/models/iphone.glb', mesh => {
   if (mesh.scene) {
-    mesh.scene.traverse(item => {
-      if (item.isMesh) {
-        if (item.name === '屏幕') {
-
-          // item.material.metalness = .5
-          screen.mesh = item;
-          screen.material = item.material;
+    mesh.scene.traverse(child => {
+      if (child instanceof THREE.Mesh && child.material instanceof THREE.MeshStandardMaterial) {
+        child.material.envMap = environmentMap;
+        child.material.envMapIntensity = 2;
+        if (child.name === '屏幕') {
+          screen.mesh = child;
+          screen.material = child.material;
           screen.videoMaterial = new THREE.MeshPhysicalMaterial({
-            map: videoTexture
+            map: videoTexture,
+            envMap: environmentMap
           });
         }
-        if (item.name.includes('边框')) {
-          console.log(item)
-          item.material.metalness = .7
+        if (child.name.includes('边框')) {
+          child.material.metalness = .8;
         }
-        if (item.name.includes('logo')) {
-          console.log(item)
-          item.material.metalness = 1
+        if (child.name.includes('logo')) {
+          child.material.metalness = 1;
         }
       }
     })
-    mesh.scene.scale.set(36, 36, 36);
-    mesh.scene.position.y = -3;
-    mesh.scene.rotation.y= -Math.PI;
-    mesh.scene.layers.set(0);
+    mesh.scene.scale.set(60, 60, 60);
+    mesh.scene.position.y = -5;
+    mesh.scene.rotation.y = -Math.PI;
     scene.add(mesh.scene);
   }
 });
 
+// 点击事件捕获
 const raycaster = new THREE.Raycaster();
 const mouse = new THREE.Vector2();
 window.addEventListener('click', event => {
