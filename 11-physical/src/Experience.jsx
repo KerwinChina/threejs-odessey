5cb4decb18a6a4355bf22fc8ceb2526d46fd94a7
dragonir <dragonirxo@outlook.com>
2023-02-01 09:12:41 +0800

    feat(physical): 🏓 完成乒乓球交互.

--- 11-physical/src/Experience.jsx
+++ 11-physical/src/Experience.jsx
@@ -1,31 +1,46 @@
-import { OrbitControls } from '@react-three/drei'
-import { Perf } from 'r3f-perf'
+import { OrbitControls } from "@react-three/drei";
+import { Perf } from "r3f-perf";
+import { Canvas } from "@react-three/fiber";
+import { useStore } from "./store.jsx";
+import PingPong from "./PingPong.jsx";
 
-export default function Experience()
-{
-    return <>
+const style = (welcome) => ({
+  color: "white",
+  display: welcome ? "block" : "none",
+  fontSize: "1.2em",
+  left: 50,
+  position: "absolute",
+  top: 50,
+});
 
-        <Perf position="top-left" />
-
+export default function Experience() {
+  const welcome = useStore((state) => state.welcome);
+  const { reset } = useStore((state) => state.api);
+  return (
+    <>
+      <Canvas
+        shadows
+        camera={{ fov: 50, position: [0, 5, 12] }}
+        onPointerMissed={() => welcome && reset(false)}
+      >
+        <Perf position="top-right" />
         <OrbitControls makeDefault />
-
-        <directionalLight castShadow position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
-        <ambientLight intensity={ 0.5 } />
-
-        <mesh castShadow position={ [ - 2, 2, 0 ] }>
-            <sphereGeometry />
-            <meshStandardMaterial color="orange" />
-        </mesh>
-
-        <mesh castShadow position={ [ 2, 2, 0 ] }>
-            <boxGeometry />
-            <meshStandardMaterial color="mediumpurple" />
-        </mesh>
-
-        <mesh receiveShadow position-y={ - 1.25 }>
-            <boxGeometry args={ [ 10, 0.5, 10 ] } />
-            <meshStandardMaterial color="greenyellow" />
-        </mesh>
-
+        <color attach="background" args={["#171720"]} />
+        <ambientLight intensity={0.5} />
+        <pointLight position={[-10, -10, -10]} />
+        <spotLight
+          position={[10, 10, 10]}
+          angle={0.3}
+          penumbra={1}
+          intensity={1}
+          castShadow
+          shadow-mapSize-width={2048}
+          shadow-mapSize-height={2048}
+          shadow-bias={-0.0001}
+        />
+        <PingPong />
+      </Canvas>
+      <div style={style(welcome)}>* click anywhere to start</div>
     </>
+  );
 }
